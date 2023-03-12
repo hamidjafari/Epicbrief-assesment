6b6701764d6ca0b4b34bcf5fac4b9e32af23b3ec
Hamidreza Jafari <hamidreza.jafari.abshouri@gmail.com>
2023-03-12 12:00:32 +0330

    build: init web client react project

---
+++ apps/web-client/src/app/App.tsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from "./App.module.scss";
+
+export function App() {
+	return <div>Welcome web-client</div>;
+}
+
+export default App;
